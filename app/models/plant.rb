5172284b8911c19c1394f9b2f8701eb8db8f0011
Joel Grant <joelmgrant@me.com>
2022-03-24 23:04:52 -0600

    Create model relationships

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,12 @@
 class Plant < ApplicationRecord
-  belongs_to :user
+  validates_presence_of :name,
+                        :type,
+                        :latin_name,
+                        :days_to_maturity,
+                        :hybrid_status,
+                        :organic,
+                        :days_relative_to_frost_date
+
+  has_many :user_plants
+  has_many :users, through: :user_plants
 end
