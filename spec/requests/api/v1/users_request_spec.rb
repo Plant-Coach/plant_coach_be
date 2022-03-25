d17bf653d1f7e51f1e3cb65bcc914b8c0d09e88a
Joel Grant <joelmgrant@me.com>
2022-03-25 16:53:44 -0600

    Add needed password information to user call

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe 'Users API' do
   describe 'POST user' do
     it 'creates a new user' do
-      body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121' }
+      body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
       post '/api/v1/users', params: body
       created_user = User.last
 
       expect(response).to be_successful
-
-      
     end
   end
 end
