0d821bb206c3f03ca94395e4f018cce8498570a0
Joel Grant <joelmgrant@me.com>
2022-03-24 22:36:55 -0600

    Tests: Create user relationship tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,9 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:password_digest) }
     it { should have_secure_password }
   end
+
+  describe 'relationships' do
+    it { should have_many(:user_plants) }
+    it { should have_many(:plants).through(:user_plants) }
+  end
 end
