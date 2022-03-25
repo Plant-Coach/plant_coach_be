877018dd65d9ad0209d59e3a693ad54dcd7dc809
Joel Grant <joelmgrant@me.com>
2022-03-25 16:05:59 -0600

    Add zip code column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_044507) do
+ActiveRecord::Schema.define(version: 2022_03_25_220519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2022_03_25_044507) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "zip_code"
   end
 
   add_foreign_key "user_plants", "plants"
