e0dece7522701fd7a6678f5cc695bbd0b03ab336
Joel Grant <joelmgrant@me.com>
2022-03-28 20:19:38 -0600

    Test: Create test for exposing a plant creation endpoint

---
+++ spec/requests/api/v1/plant_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Plant API Endpoints' do
+  describe 'POST /plants' do
+    it 'creates a new plant in the database' do
+      plant = {plant_type: "Tomato", name: "Sungold", days_relative_to_frost_date: 14, days_to_maturity: 54, hybrid_status: 1}
+      post '/api/v1/plants', params: plant
+      result = JSON.parse(response.body, symbolize_names: true)
+      require 'pry'; binding.pry
+    end
+  end
+end
