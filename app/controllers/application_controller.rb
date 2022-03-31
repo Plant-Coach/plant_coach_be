c4e35cb114a581fd22c036e94f03a35eb6ce1bce
Joel Grant <joelmgrant@me.com>
2022-03-31 13:31:33 -0600

    Create auth_header method for jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,11 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'secret')
   end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
   def decoded_token(token)
     if auth_header
       token = auth_header.split(' ')[1]
