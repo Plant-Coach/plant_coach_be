5f8284b90e29e9b4130ad0ca402e93e688b67046
Joel Grant <joelmgrant@me.com>
2022-03-31 17:44:04 -0600

    Modify sessions to be secured with JWT

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe 'User Sessions' do
     it 'logs in a user' do
       body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
       post '/api/v1/users', params: body
+      user_response = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
 
       login_params = { email: 'joel@plantcoach.com', password: '12345' }
-      post '/api/v1/sessions', params: login_params
+      post '/api/v1/sessions', params: login_params, headers: { Authorization: "Bearer #{user_response[:jwt]}" }
 
       expect(response).to be_successful
     end
@@ -17,11 +18,12 @@ RSpec.describe 'User Sessions' do
     it 'returns an error if the password is bad' do
       body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
       post '/api/v1/users', params: body
+      user_response = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
 
       login_params = { email: 'joel@plantcoach.com', password: 'WRONG PASSWORD' }
-      post '/api/v1/sessions', params: login_params
+      post '/api/v1/sessions', params: login_params, headers: { Authorization: "Bearer #{user_response[:jwt]}" }
 
       result = JSON.parse(response.body, symbolize_names: true)
 
@@ -31,10 +33,11 @@ RSpec.describe 'User Sessions' do
     it 'returns an error if the user doesnt exist' do
       body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
       post '/api/v1/users', params: body
+      user_response = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
 
       non_existent_user = {email: 'joe12345@shmo.com', password: 'ABCDE'}
-      post '/api/v1/sessions', params: non_existent_user
+      post '/api/v1/sessions', params: non_existent_user, headers: { Authorization: "Bearer #{user_response[:jwt]}" }
 
       result = JSON.parse(response.body, symbolize_names: true)
 
