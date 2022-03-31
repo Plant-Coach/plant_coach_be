3cd58a4e9d9289bc6b6ce802454cb4663c7a2fc5
Joel Grant <joelmgrant@me.com>
2022-03-31 17:44:32 -0600

    Modify user plants to require jwt authentication to work

--- spec/requests/api/v1/user_plants_request_spec.rb
+++ spec/requests/api/v1/user_plants_request_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe 'User Plants API Endpoint' do
   describe 'POST user plants' do
     it 'creates new plant that the user will be planting' do
+      body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
+      post '/api/v1/users', params: body
+      user_response = JSON.parse(response.body, symbolize_names: true)
+
       plant = Plant.create!(plant_type: "Tomato", name: "Sungold", days_relative_to_frost_date: 14, days_to_maturity: 54, hybrid_status: 1)
       user = User.create!(name: "Joel User", email: 'joel@123.com', password: "12345", zip_code: 80123)
 
-      post '/api/v1/user_plants', params: { user_id: user.id, plant_id: plant.id}
+      post '/api/v1/user_plants', params: { user_id: user.id, plant_id: plant.id}, headers: { Authorization: "Bearer #{user_response[:jwt]}" }
       result = JSON.parse(response.body, symbolize_names: true)
       new_plant = UserPlant.last
 
@@ -16,10 +20,14 @@ RSpec.describe 'User Plants API Endpoint' do
     end
 
     it 'will return a json error message if there was a problem' do
+      body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
+      post '/api/v1/users', params: body
+      user_response = JSON.parse(response.body, symbolize_names: true)
+
       plant = Plant.create!(plant_type: "Tomato", name: "Sungold", days_relative_to_frost_date: 14, days_to_maturity: 54, hybrid_status: 1)
       user = User.create!(name: "Joel User", email: 'joel@123.com', password: "12345", zip_code: 80123)
 
-      post '/api/v1/user_plants', params: { user_id: nil, plant_id: plant.id}
+      post '/api/v1/user_plants', params: { user_id: nil, plant_id: plant.id}, headers: { Authorization: "Bearer #{user_response[:jwt]}" }
       result = JSON.parse(response.body, symbolize_names: true)
 
       expect(response.status).to eq(400)
@@ -28,11 +36,15 @@ RSpec.describe 'User Plants API Endpoint' do
 
   describe 'DELETE /user_plants' do
     it 'removes the plant from the users list of plants' do
+      body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
+      post '/api/v1/users', params: body
+      user_response = JSON.parse(response.body, symbolize_names: true)
+      
       plant = Plant.create!(plant_type: "Tomato", name: "Sungold", days_relative_to_frost_date: 14, days_to_maturity: 54, hybrid_status: 1)
       user = User.create!(name: "Joel User", email: 'joel@123.com', password: "12345", zip_code: 80123)
       user_plant = UserPlant.create(user_id: user.id, plant_id: plant.id)
 
-      delete "/api/v1/user_plants/#{user_plant.id}" # Would like to refactor this to use params but it does not like them for some reason.
+      delete "/api/v1/user_plants/#{user_plant.id}", headers: { Authorization: "Bearer #{user_response[:jwt]}" } # Would like to refactor this to use params but it does not like them for some reason.
       result = JSON.parse(response.body, symbolize_names: true)
 
       expect(result[:data][:attributes][:id]).to eq(user_plant.id)
