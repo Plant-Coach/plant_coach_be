bd427b56d90b1c0e31265db0cacbe3a62e93b52f
Joel Grant <joelmgrant@me.com>
2022-03-31 13:31:58 -0600

    Add aditional verification methods for authenticated users with JWT

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
+
   def passwords_dont_match
     params[:password] != params[:password_confirmation]
   end
@@ -10,6 +12,7 @@ class ApplicationController < ActionController::API
   def email_formatted_incorrectly(user)
     user.errors.messages[:email]
   end
+
   def encode_token(payload)
     JWT.encode(payload, 'secret')
   end
@@ -28,5 +31,20 @@ class ApplicationController < ActionController::API
         nil
       end
     end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in'}, status: :unauthorized unless logged_in?
   end
 end
