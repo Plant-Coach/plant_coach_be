4388a1f6c72792f594ca3e509e9aa861689f7781
Joel Grant <joelmgrant@me.com>
2022-03-31 17:43:48 -0600

    Modify plant request tests to be authenticated calls

--- spec/requests/api/v1/plant_request_spec.rb
+++ spec/requests/api/v1/plant_request_spec.rb
@@ -11,8 +11,22 @@ RSpec.describe 'Plant API Endpoints' do
 
   describe 'PATCH /plants' do
     it 'updates an existing plant with new attributes' do
+      body = {
+        name: 'Joel Grant',
+        email: 'joel@plantcoach.com',
+        zip_code: '80121',
+        password: '12345',
+        password_confirmation: '12345'
+      }
+      post '/api/v1/users', params: body
+      created_user = User.last
+
+      expect(response).to be_successful
+
+      user_response = JSON.parse(response.body, symbolize_names: true)
+
       plant = Plant.create(plant_type: "Tomato", name: "Sungold", days_relative_to_frost_date: 14, days_to_maturity: 54, hybrid_status: 1)
-      patch "/api/v1/plants/#{plant.id}", params: { days_to_maturity: 61 }
+      patch "/api/v1/plants/#{plant.id}", params: { days_to_maturity: 61 }, headers: { Authorization: "Bearer #{user_response[:jwt]}" }
       result = JSON.parse(response.body, symbolize_names: true)
 
       expect(result[:data][0][:attributes][:days_to_maturity]).to eq(61)
@@ -21,21 +35,48 @@ RSpec.describe 'Plant API Endpoints' do
 
   describe 'DELETE /plants' do
     it 'removes a plant from the list of available plants' do
+      body = {
+        name: 'Joel Grant',
+        email: 'joel@plantcoach.com',
+        zip_code: '80121',
+        password: '12345',
+        password_confirmation: '12345'
+      }
+      post '/api/v1/users', params: body
+      created_user = User.last
+
+      expect(response).to be_successful
+
+      user_response = JSON.parse(response.body, symbolize_names: true)
+
       plant1 = Plant.create(plant_type: "Tomato", name: "Sungold", days_relative_to_frost_date: 14, days_to_maturity: 54, hybrid_status: 1)
       plant2 = Plant.create(plant_type: "Pepper", name: "Jalafuego", days_relative_to_frost_date: 14, days_to_maturity: 65, hybrid_status: 1)
       plant3 = Plant.create(plant_type: "Radish", name: "French Breakfast", days_relative_to_frost_date: 30, days_to_maturity: 21, hybrid_status: 1)
 
-      delete "/api/v1/plants/#{plant3.id}"
+      delete "/api/v1/plants/#{plant3.id}", headers: { Authorization: "Bearer #{user_response[:jwt]}" }
       result = JSON.parse(response.body, symbolize_names: true)
 
       expect(Plant.find_by(id: plant3.id)).to be nil
     end
 
     it 'returns an error if the plant cant be found' do
+      body = {
+        name: 'Joel Grant',
+        email: 'joel@plantcoach.com',
+        zip_code: '80121',
+        password: '12345',
+        password_confirmation: '12345'
+      }
+      post '/api/v1/users', params: body
+      created_user = User.last
+
+      expect(response).to be_successful
+
+      user_response = JSON.parse(response.body, symbolize_names: true)
       plant1 = Plant.create(plant_type: "Tomato", name: "Sungold", days_relative_to_frost_date: 14, days_to_maturity: 54, hybrid_status: 1)
       plant2 = Plant.create(plant_type: "Pepper", name: "Jalafuego", days_relative_to_frost_date: 14, days_to_maturity: 65, hybrid_status: 1)
       plant3 = Plant.create(plant_type: "Radish", name: "French Breakfast", days_relative_to_frost_date: 30, days_to_maturity: 21, hybrid_status: 1)
-      delete "/api/v1/plants/999999"
+      delete "/api/v1/plants/999999", headers: { Authorization: "Bearer #{user_response[:jwt]}" }
       result = JSON.parse(response.body, symbolize_names: true)
       expect(result[:error]).to eq("Something happened!")
     end
