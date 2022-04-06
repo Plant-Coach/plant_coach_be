5fbbc1a9411e266ca0a65552c864f5569fb58ef1
Joel Grant <joelmgrant@me.com>
2022-04-06 11:06:20 -0600

    Modify user plants controller to work with the JWT process

--- app/controllers/api/v1/user_plants_controller.rb
+++ app/controllers/api/v1/user_plants_controller.rb
@@ -6,20 +6,20 @@ class Api::V1::UserPlantsController < ApplicationController
 
   def create
     user_plant = UserPlant.create(
-      user_id: params[:user_id],
+      user_id: @user.id,
       plant_id: params[:plant_id]
     )
 
     if user_plant.save
-      user = User.find(params[:user_id])
       plant = Plant.find(params[:plant_id])
-      render json: UserPlantSerializer.format(user, plant)
+      render json: UserPlantSerializer.format(@user, plant)
     else
       render json: UserPlantSerializer.error("There was a problem."), status: 400
     end
   end
 
   def destroy
+    require 'pry'; binding.pry
     user_plant = UserPlant.find(params[:id])
     if user_plant != nil
       result = UserPlant.destroy(user_plant.id)
