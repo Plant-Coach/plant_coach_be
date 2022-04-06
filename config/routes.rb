37884b9fd4701cc2117398a70b52437bc9f93659
Joel Grant <joelmgrant@me.com>
2022-04-06 11:05:14 -0600

    Add user_plants index action

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :update]
-      resources :user_plants, only: [:create, :destroy]
+      resources :user_plants, only: [:index, :create, :destroy]
       resources :sessions, only: [:create]
       resources :forecast, only: [:create]
       resources :plants, only: [:create, :update, :destroy]
