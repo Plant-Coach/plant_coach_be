763823bb5aa3b7687bc262cdded180dfae36db0e
Joel Grant <joelmgrant@me.com>
2022-04-14 09:55:32 -0600

    Test: Create API Endpoint for frost date data

--- spec/requests/api/v1/frost_date_request_spec.rb
+++ spec/requests/api/v1/frost_date_request_spec.rb
@@ -3,10 +3,35 @@ require 'rails_helper'
 RSpec.describe 'Frost Date API Endpoint' do
   describe 'GET /frost_data' do
     it 'returns the the spring and fall frost dates in addition to more details location data' do
-      user_zip_code = { zip_code: 80124 }
-      get '/api/v1/frost_dates', params: user_zip_code
+      body = {
+        name: 'Joel Grant',
+        email: 'joel@plantcoach.com',
+        zip_code: '80121',
+        password: '12345',
+        password_confirmation: '12345'
+      }
+      post '/api/v1/users', params: body
+      user_response = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/frost_dates', headers: {
+        Authorization: "Bearer #{user_response[:jwt]}"
+      }
+
       result = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+
+      expect(response).to be_successful
+
+      expect(result).to be_a Hash
+      expect(result).to have_key(:data)
+
+      expect(result[:data][:attributes]).to be_a Hash
+      expect(result[:data][:attributes]).to have_key(:id)
+      expect(result[:data][:attributes]).to have_key(:zip_code)
+      expect(result[:data][:attributes]).to have_key(:location_name)
+      expect(result[:data][:attributes]).to have_key(:lat)
+      expect(result[:data][:attributes]).to have_key(:lon)
+      expect(result[:data][:attributes]).to have_key(:spring_frost)
+      expect(result[:data][:attributes]).to have_key(:fall_frost)
     end
   end
 end
