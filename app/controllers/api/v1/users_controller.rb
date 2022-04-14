397a3d8b1affdd7399df3ac32ca20d51b9dcf097
Joel Grant <joelmgrant@me.com>
2022-04-14 16:42:55 -0600

    Feat: Create endpoint that provides a users data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
+  def index
+    if !@user.nil?
+      render json: UserSerializer.new(@user), status: 200
+    end
+  end
+
   def create
     user = User.create(user_params)
     if user.valid?
