5e72eb2b389f4a9879c358123aff60b5a6263a03
Joel Grant <joelmgrant@me.com>
2022-04-14 09:55:58 -0600

    Feat: Create API endpoint for frost dates

---
+++ app/facades/frost_date_facade.rb
@@ -0,0 +1,9 @@
+class FrostDateFacade
+  def self.get_frost_dates(zip_code)
+    frost_date_data = FrostDateService.get_frost_dates(zip_code)
+
+    FrostDates.new(frost_date_data)
+
+
+  end
+end
--- app/microservices/frost_date_service.rb
+++ app/microservices/frost_date_service.rb
@@ -1,9 +1,9 @@
 class FrostDateService
   def self.get_frost_dates(zip_code)
-    conn = Faraday.new(url: "https://glacial-fjord-58347.herokuapp.com")
-    response = conn.post("/api/v1/forecast") do |req|
-      req.body = { "location": zip_code }
-    end
+    conn = Faraday.new(url: "https://glacial-fjord-58347.herokuapp.com",
+      params: { zip_code: zip_code }
+    )
+    response = conn.get("/api/v1/frost")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
