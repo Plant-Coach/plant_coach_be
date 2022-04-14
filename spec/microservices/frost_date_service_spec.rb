3af21ffe2543751afde19f09dd9e6e1d935fdf9c
Joel Grant <joelmgrant@me.com>
2022-04-14 09:58:16 -0600

    Remove incorrect assertions

--- spec/microservices/frost_date_service_spec.rb
+++ spec/microservices/frost_date_service_spec.rb
@@ -9,24 +9,8 @@ RSpec.describe FrostDateService do
       expect(data).to be_a Hash
       expect(data).to have_key(:data)
 
-      expect(data[:data]).to be_an Array
+      expect(data[:data]).to be_an Hash
 
-      data[:data].each do |weather|
-        expect(weather).to be_a Hash
-        expect(weather).to have_key(:id)
-        expect(weather).to have_key(:type)
-        expect(weather).to have_key(:attributes)
-
-        expect(weather[:attributes]).to be_a Hash
-        expect(weather[:attributes]).to have_key(:date)
-        expect(weather[:attributes]).to have_key(:sunrise)
-        expect(weather[:attributes]).to have_key(:sunset)
-        expect(weather[:attributes]).to have_key(:high)
-        expect(weather[:attributes]).to have_key(:low)
-        expect(weather[:attributes]).to have_key(:humidity)
-        expect(weather[:attributes]).to have_key(:wind)
-        expect(weather[:attributes]).to have_key(:weather)
-      end
     end
   end
 end
