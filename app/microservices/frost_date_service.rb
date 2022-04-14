23d28489d0ad5540d3d61c396766416f6862429a
Joel Grant <joelmgrant@me.com>
2022-04-14 09:26:24 -0600

    Consume frost date microservice endpoint

---
+++ app/microservices/frost_date_service.rb
@@ -0,0 +1,9 @@
+class FrostDateService
+  def self.get_frost_dates(zip_code)
+    conn = Faraday.new(url: "https://glacial-fjord-58347.herokuapp.com")
+    response = conn.post("/api/v1/forecast") do |req|
+      req.body = { "location": zip_code }
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
