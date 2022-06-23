6d7102622e4ec4773dac936e1d3886ae44eea92b
Joel Grant <joelmgrant@me.com>
2022-06-23 13:11:18 -0600

    Remove unused validation test

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Plant, type: :model do
     it { should validate_presence_of(:days_to_maturity) }
     it { should validate_presence_of(:hybrid_status) }
     it { should validate_presence_of(:days_relative_to_frost_date) }
-    it { should validate_presence_of(:organic) }
     it { should validate_uniqueness_of(:name) }
   end
 
