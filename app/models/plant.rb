e55380920197d5267efacf724bf39664f805ced0
Joel Grant <joelmgrant@me.com>
2022-06-23 10:13:21 -0600

    Add missing comma

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,9 +3,12 @@ class Plant < ApplicationRecord
                         :plant_type,
                         :days_to_maturity,
                         :hybrid_status,
-                        :days_relative_to_frost_date
+                        :days_relative_to_frost_date,
+                        :organic
+  validates_uniqueness_of :name
 
   belongs_to :user
 
   enum hybrid_status: [:open_pollinated, :f1]
+  enum organic: [true, false]
 end
