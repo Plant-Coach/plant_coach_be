2f2ddecd77dc8d38330404ff33199d3a9a32f51d
Joel Grant <joelmgrant@me.com>
2022-06-23 10:10:56 -0600

    Migrate new organic column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_154321) do
+ActiveRecord::Schema.define(version: 2022_06_23_160955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_06_23_154321) do
     t.integer "days_relative_to_frost_date"
     t.string "plant_type"
     t.bigint "user_id"
+    t.boolean "organic"
     t.index ["user_id"], name: "index_plants_on_user_id"
   end
 
