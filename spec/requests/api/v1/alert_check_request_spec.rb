461092aa21c2b6dc941330f730b7b68499b3bbc7
Joel Grant <joelmgrant@me.com>
2022-06-30 21:50:27 -0600

    Test: Create a weather alert check process

---
+++ spec/requests/api/v1/alert_check_request_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'Weather Alert API Endpoints' do
+  describe 'POST /alert_check endpoint' do
+    it 'allows a microservice request to be made for a list of zip codes' do
+      body1 = {
+        name: 'Joel Grant1',
+        email: 'joel1@plantcoach.com',
+        zip_code: '80123',
+        password: '12345',
+        password_confirmation: '12345'
+      }
+      post '/api/v1/users', params: body1
+
+      body2 = {
+        name: 'Joel Grant2',
+        email: 'joel2@plantcoach.com',
+        zip_code: '80124',
+        password: '12345',
+        password_confirmation: '12345'
+      }
+      post '/api/v1/users', params: body2
+
+      body3 = {
+        name: 'Joel Grant3',
+        email: 'joel3@plantcoach.com',
+        zip_code: '80125',
+        password: '12345',
+        password_confirmation: '12345'
+      }
+      post '/api/v1/users', params: body3
+
+      post '/api/v1/alert_check', headers: { Auth: "qwerty" }
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      result.each do |location_data|
+        expect(location_data).to have_key(:id)
+        expect(location_data).to have_key(:zip)
+      end
+    end
+  end
+
+end
