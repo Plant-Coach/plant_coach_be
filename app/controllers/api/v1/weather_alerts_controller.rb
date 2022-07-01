1b840884edcabd21fffac506809113791ed23609
Joel Grant <joelmgrant@me.com>
2022-07-01 15:52:08 -0600

    Comment out code to avoid emails being sent temporarily

--- app/controllers/api/v1/weather_alerts_controller.rb
+++ app/controllers/api/v1/weather_alerts_controller.rb
@@ -2,13 +2,14 @@ class Api::V1::WeatherAlertsController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    if request.headers['Auth'] == ENV['microservice_key']
-      request.headers['Alert'].each do |alert_payload|
-        decoded_user = JWT.decode(alert_payload[:id], 'secret', true, algorithm: 'HS256')
-        user = User.find(decoded_user[0]['user_id'])
-        alerts = alert_payload[:alerts].map { |alert| Alert.new(alert) }
-        WeatherAlerterMailer.inform(user, alerts).deliver_now
-      end
-    end
+    # Intentionally commented out functionality until DB users can be setup.
+    # if request.headers['Auth'] == ENV['microservice_key']
+    #   request.headers['Alert'].each do |alert_payload|
+    #     decoded_user = JWT.decode(alert_payload[:id], 'secret', true, algorithm: 'HS256')
+    #     user = User.find(decoded_user[0]['user_id'])
+    #     alerts = alert_payload[:alerts].map { |alert| Alert.new(alert) }
+    #     WeatherAlerterMailer.inform(user, alerts).deliver_now
+    #   end
+    # end
   end
 end
