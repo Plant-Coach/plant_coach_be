00357f14fbc95608d8d3cb8368b05313e5c456ce
Joel Grant <joelmgrant@me.com>
2022-07-06 16:48:49 -0600

    Remove commented out code

--- app/controllers/api/v1/garden_reminder_controller.rb
+++ app/controllers/api/v1/garden_reminder_controller.rb
@@ -2,13 +2,10 @@ class Api::V1::GardenReminderController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    # require 'pry'; binding.pry 
-    # Intentionally commented out functionality until DB users can be setup.
     if request.headers['Auth'] == ENV['microservice_key']
       request.headers['Alert'].each do |alert_payload|
         decoded_user = JWT.decode(alert_payload[:id], 'secret', true, algorithm: 'HS256')
         user = User.find(decoded_user[0]['user_id'])
-        # require 'pry'; binding.pry
         reminders = alert_payload[:reminder].map { |reminder| Reminder.new(reminder) }
         GardenReminderMailer.inform(user, reminders).deliver_now
       end
