68cf82e1caef0a2d42025c93a644464a20774e09
Joel Grant <joelmgrant@me.com>
2022-07-06 16:41:13 -0600

    Rename and reformat mail alerts as garden reminders

---
+++ app/mailers/garden_reminder_mailer.rb
@@ -0,0 +1,14 @@
+class GardenReminderMailer < ApplicationMailer
+  def inform(user, reminder_list)
+    @email = user.email
+    @name = user.name
+    @zip_code = user.zip_code
+    @reminders = reminder_list
+
+    mail(
+      reply_to: "test@plants.asdf",
+      to: @email,
+      subject: "#{@name}, you have a reminder to do!"
+    )
+  end
+end
