ed1dc4a9529b3554333b132ce5810907332abbd2
Joel Grant <joelmgrant@me.com>
2022-07-06 16:59:15 -0600

    Name change to garden reminder

--- spec/mailers/garden_reminder_spec.rb
+++ spec/mailers/garden_reminder_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe WeatherAlerterMailer, type: :mailer do
+RSpec.describe GardenReminderMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
 end
