395c60774ec58e8f7855b24edf01e3f03819ab1e
Joel Grant <joelmgrant@me.com>
2022-09-10 18:58:14 -0600

    Remove old methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,6 @@ class User < ApplicationRecord
   has_many :plants
   has_many :garden_plants
 
-  def find_users_plants
-    plants.where('user_plants.user_id = ?', "#{self.id}")
-  end
-
-  def find_user_plant_by_plant_id(id)
-    user_plants.where(plant_id: id).first
-  end
-
   def self.all_zip_codes
     all.select(:id, :zip_code)
   end
