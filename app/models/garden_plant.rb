1e2fc09a86235112f8542e0300ac52846101b7df
Joel Grant <joelmgrant@me.com>
2022-09-10 18:59:54 -0600

    Comment all models

--- app/models/garden_plant.rb
+++ app/models/garden_plant.rb
@@ -1,13 +1,20 @@
+# GardenPlants are objects that the user has decided to plant.
 class GardenPlant < ApplicationRecord
+  # Standard validations that are required to save the record.
   validates_presence_of :name,
                         :plant_type,
                         :days_to_maturity,
                         :hybrid_status,
                         :days_relative_to_frost_date
+  # "Inclusion" validates that the attribute belongs to an enumerable object.
   validates_inclusion_of :organic, in: [true, false]
+  # Records must be unique according to name, but only unique for those that
+  # belong to each user (aka "user_id").
   validates :name, presence: true, uniqueness: { scope: :user_id }
 
+  # GardenPlants belong to a user.
   belongs_to :user
 
+  # Hybrid Status can only be categorized as these two enumerables.
   enum hybrid_status: [:open_pollinated, :f1]
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :password_digest, presence: true
   validates_uniqueness_of :email
+  # Only allowed formatting for email addresses.
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates_presence_of :password_digest
+  # Bcrypt method to encrypt the password digest (above).
   has_secure_password
-
+  # Users can have plants.
   has_many :plants
+  # USers can have garden plants.
   has_many :garden_plants
 
+  # Returns the zip codes of all users in the database.  The corresponding
+  # ids are also returned.
   def self.all_zip_codes
     all.select(:id, :zip_code)
   end
