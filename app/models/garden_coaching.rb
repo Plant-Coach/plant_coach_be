54cbd227b28234653ce8b5b004ec3453b0ae8f03
Joel Grant <joelmgrant@me.com>
2022-09-23 18:52:34 -0600

    Feat: Create garden coaching table and join tables

--- app/models/garden_coaching.rb
+++ app/models/garden_coaching.rb
@@ -1,3 +1,6 @@
 class GardenCoaching < ApplicationRecord
   validates_presence_of :title, :description, :days_to_remind
+
+  has_many :garden_user_coachings
+  has_many :users, through: :garden_user_coachings
 end
