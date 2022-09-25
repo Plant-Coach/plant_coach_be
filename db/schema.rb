b154f2a590d43220504047701afad3930ff734b8
Joel Grant <joelmgrant@me.com>
2022-09-24 20:11:48 -0600

    Add planting guide migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_004221) do
+ActiveRecord::Schema.define(version: 2022_09_25_020304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 2022_09_24_004221) do
     t.datetime "updated_at", null: false
     t.index ["garden_coaching_id"], name: "index_garden_user_coachings_on_garden_coaching_id"
     t.index ["user_id"], name: "index_garden_user_coachings_on_user_id"
+  end
+
+  create_table "planting_guides", force: :cascade do |t|
+    t.string "plant_type"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "plants", force: :cascade do |t|
