02fad11a4bf1936041e04844d323fb042fb8d356
Joel Grant <joelmgrant@me.com>
2022-09-25 13:24:15 -0600

    Add additional validation test for new attribute

--- spec/models/seed_default_data_spec.rb
+++ spec/models/seed_default_data_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe SeedDefaultData, type: :model do
     it { should validate_presence_of(:plant_type) }
     it { should validate_presence_of(:days_to_maturity) }
     it { should validate_presence_of(:seed_days_to_transplant) }
+    it { should validate_presence_of(:days_relative_to_frost_date) }
   end
 
   describe 'relationships' do
