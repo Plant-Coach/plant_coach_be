7f63ca035d363fa5d4b8cdecb8d5d39c59480240
Joel Grant <joelmgrant@me.com>
2022-09-25 11:13:03 -0600

    Test: Create seed default data validation tests

--- spec/models/seed_default_data_spec.rb
+++ spec/models/seed_default_data_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe SeedDefaultData, type: :model do
   describe 'validations' do
-
+    it { should validate_presence_of(:plant_type) }
+    it { should validate_presence_of(:days_to_maturity) }
+    it { should validate_presence_of(:seed_days_to_transplant) }
   end
-  
+
   describe 'relationships' do
 
   end
