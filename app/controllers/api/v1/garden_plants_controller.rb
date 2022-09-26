2bbb8a88f81e28787af206ac5e6b0ef6f7b60993
Joel Grant <joelmgrant@me.com>
2022-09-25 21:30:03 -0600

    Add option for user to plant now or wait

--- app/controllers/api/v1/garden_plants_controller.rb
+++ app/controllers/api/v1/garden_plants_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::GardenPlantsController < ApplicationController
 
   def create
     plant_result = Plant.find_by_id(params[:plant_id])
-    garden_plant = @user.create_garden_plant(plant_result, params[:start_from_seed])
+    garden_plant = @user.create_garden_plant(plant_result, params[:start_from_seed], params[:plant_now])
     render json: GardenPlantSerializer.new(garden_plant)
   end
 
