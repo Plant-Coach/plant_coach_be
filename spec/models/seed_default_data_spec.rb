d301d77d150fa1a1e4d3f7a67fb1c3b897cb4d2d
Joel Grant <joelmgrant@me.com>
2022-09-25 21:28:15 -0600

    Modified attribute

--- spec/models/seed_default_data_spec.rb
+++ spec/models/seed_default_data_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe SeedDefaultData, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:plant_type) }
     it { should validate_presence_of(:days_to_maturity) }
-    it { should validate_presence_of(:seed_days_to_transplant) }
+    it { should validate_presence_of(:seedling_days_to_transplant) }
     it { should validate_presence_of(:days_relative_to_frost_date) }
     it { should validate_presence_of(:direct_seed) }
   end
