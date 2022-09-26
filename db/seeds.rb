eb2804ebffa6191e5930d5933f10875cd865c6fb
Joel Grant <joelmgrant@me.com>
2022-09-25 19:49:03 -0600

    Add new attributes to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -133,29 +133,34 @@ tomato_seed = SeedDefaultData.create(
   plant_type: "Tomato",
   days_to_maturity: 55,
   seed_days_to_transplant: 49,
-  days_relative_to_frost_date: 14
+  days_relative_to_frost_date: 14,
+  direct_seed: :no
 )
 pepper_seed = SeedDefaultData.create(
   plant_type: "Pepper",
   days_to_maturity: 64,
   seed_days_to_transplant: 49,
-  days_relative_to_frost_date: 14
+  days_relative_to_frost_date: 14,
+  direct_seed: :no
 )
 eggplant_seed = SeedDefaultData.create(
   plant_type: "Eggplant",
   days_to_maturity: 68,
   seed_days_to_transplant: 49,
-  days_relative_to_frost_date: 14
+  days_relative_to_frost_date: 14,
+  direct_seed: :no
 )
 romaine_seed = SeedDefaultData.create(
   plant_type: "Romaine Lettuce",
   days_to_maturity: 35,
   seed_days_to_transplant: 14,
-  days_relative_to_frost_date: -28
+  days_relative_to_frost_date: -28,
+  direct_seed: :yes
 )
 green_bean_seed = SeedDefaultData.create(
   plant_type: "Green Bean",
   days_to_maturity: 52,
   seed_days_to_transplant: 14,
-  days_relative_to_frost_date: 0
+  days_relative_to_frost_date: 0,
+  direct_seed: :yes
 )
