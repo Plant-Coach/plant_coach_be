b6cb35c04b93e84f7644284687ba0cbc0ebd096c
Joel Grant <joelmgrant@me.com>
2022-09-25 21:29:11 -0600

    Correct erroneous attribute name

--- app/models/seed_default_data.rb
+++ app/models/seed_default_data.rb
@@ -1,7 +1,7 @@
 class SeedDefaultData < ApplicationRecord
   validates_presence_of :plant_type,
                         :days_to_maturity,
-                        :seed_days_to_transplant,
+                        :seedling_days_to_transplant,
                         :days_relative_to_frost_date,
                         :direct_seed
 
