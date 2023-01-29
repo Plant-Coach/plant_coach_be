e703783bf04ebd00658121d670f08db59890a195
Joel Grant <joelmgrant@me.com>
2023-01-28 22:30:39 -0700

    Refactor sessions test suite

--- spec/requests/api/v1/sessions_request_spec.rb
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -1,12 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'User Sessions' do
+RSpec.describe 'User Sessions', :vcr do
+
+
+  before(:each) do
+    post '/api/v1/users', params: body
+  end
+
+  let(:body) { { name: 'Joel Grant', email: 'joel@plantcoach.com',
+    zip_code: '80121', password: '12345', password_confirmation: '12345' } }
+
+  let(:user_response) { JSON.parse(response.body, symbolize_names: true) }
+
   describe 'POST /sessions' do
     it 'logs in a user' do
-      body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
-      post '/api/v1/users', params: body
-      user_response = JSON.parse(response.body, symbolize_names: true)
-
       expect(response).to be_successful
 
       login_params = { email: 'joel@plantcoach.com', password: '12345' }
@@ -16,10 +23,6 @@ RSpec.describe 'User Sessions' do
     end
 
     it 'returns an error if the password is bad' do
-      body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
-      post '/api/v1/users', params: body
-      user_response = JSON.parse(response.body, symbolize_names: true)
-
       expect(response).to be_successful
 
       login_params = { email: 'joel@plantcoach.com', password: 'WRONG PASSWORD' }
@@ -31,9 +34,6 @@ RSpec.describe 'User Sessions' do
     end
 
     it 'returns an error if the user doesnt exist' do
-      body = { name: 'Joel Grant', email: 'joel@plantcoach.com', zip_code: '80121', password: '12345', password_confirmation: '12345' }
-      post '/api/v1/users', params: body
-      user_response = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
 
       non_existent_user = {email: 'joe12345@shmo.com', password: 'ABCDE'}
