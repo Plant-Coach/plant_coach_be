461940d41ea23760cc1db6c1885ef80054853079
Joel Grant <joelmgrant@me.com>
2023-01-28 19:18:21 -0700

    Set up webmock

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,16 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Users API' do
+  before(:each) do
+    body = {
+      name: 'Joel Grant',
+      email: 'joel@plantcoach.com',
+      zip_code: '80121',
+      password: '12345',
+      password_confirmation: '12345'
+    }
+    post '/api/v1/users', params: body
+  end
   describe 'POST user' do
     it 'creates a new user' do
-      body = {
-        name: 'Joel Grant',
-        email: 'joel@plantcoach.com',
-        zip_code: '80121',
-        password: '12345',
-        password_confirmation: '12345'
-      }
-      post '/api/v1/users', params: body
+      # body = {
+      #   name: 'Joel Grant',
+      #   email: 'joel@plantcoach.com',
+      #   zip_code: '80121',
+      #   password: '12345',
+      #   password_confirmation: '12345'
+      # }
+
+      # post '/api/v1/users', params: body
+      binding.pry
       created_user = User.last
 
       expect(response).to be_successful
