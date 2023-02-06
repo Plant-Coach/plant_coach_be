d5d1c62c84317536c9dd799a66f9d657bd6e6242
Joel Grant <joelmgrant@me.com>
2023-02-05 22:00:00 -0700

    fix: update the usefulness of comments

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,7 +1,6 @@
 # Plants are considered records that each user wants to have in their personal
 # database.
 class Plant < ApplicationRecord
-  # Standard mandatory plant attributes.
   validates_presence_of :name,
                         :plant_type,
                         :days_to_maturity,
@@ -9,11 +8,10 @@ class Plant < ApplicationRecord
                         :days_relative_to_frost_date
   # "Inclusion" validates that the attribute belongs to an enumerable object.
   # validates_inclusion_of :organic, in: [true, false]
-  # Records must be unique according to name, but only unique for those that
-  # belong to each user (aka "user_id").
+  # Plant records must be unique according to name, but only unique for those that
+  # belong to each user, based on user_id.
   validates :name, presence: true, uniqueness: { scope: :user_id }
 
-  # Plants belong to a user.
   belongs_to :user
 
   enum hybrid_status: [:unknown, :open_pollinated, :f1]
@@ -22,7 +20,9 @@ class Plant < ApplicationRecord
   after_initialize :set_defaults, unless: :skip_callbacks
 
   private
-
+  
+  # A user is not expected to know all of the details about a plant.  This fills
+  # in some of the blanks with default data.
   def set_defaults
     plant_defaults = SeedDefaultData.find_by(plant_type: plant_type)
     self.update(days_to_maturity: plant_defaults.days_to_maturity) if days_to_maturity.nil?
