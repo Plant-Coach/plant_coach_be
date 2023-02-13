6680c4ad8df6634fca3955fda461f5614e400e36
Joel Grant <joelmgrant@me.com>
2023-02-12 19:24:27 -0700

    Modify some user tests and update vcr expiration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,5 +79,5 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.default_cassette_options = { re_record_interval: 7.days }
+  config.default_cassette_options = { re_record_interval: 120.days }
 end
