5f182375fd04d446594db893a798dd6d36b3856e
Joel Grant <joelmgrant@me.com>
2023-06-19 16:00:26 -0600

    Update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_06_040922) do
+ActiveRecord::Schema.define(version: 2023_06_19_214123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2023_06_06_040922) do
     t.datetime "updated_at", null: false
     t.date "harvest_start"
     t.date "harvest_finish"
+    t.integer "harvest_period", default: 0, null: false
     t.index ["user_id"], name: "index_garden_plants_on_user_id"
   end
 
@@ -73,6 +74,7 @@ ActiveRecord::Schema.define(version: 2023_06_06_040922) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.integer "harvest_period", default: 0, null: false
     t.index ["user_id"], name: "index_plants_on_user_id"
   end
 
