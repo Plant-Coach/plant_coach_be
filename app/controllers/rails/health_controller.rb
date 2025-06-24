0783c52d18cd055f158b04e08d34b0582b684774
Joel Grant <joelmgrant@me.com>
2025-06-24 14:14:31 -0600

    update controller action

--- app/controllers/rails/health_controller.rb
+++ app/controllers/rails/health_controller.rb
@@ -1,6 +1,6 @@
 class Rails::HealthController < ApplicationController
-  skip_before_action :authorized, only: [:index]
-  def index
+  skip_before_action :authorized, only: [:show]
+  def show
     render json: { status: 'UP' }, status: :ok
   end
 end
